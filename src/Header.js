9dcae80c0f0af838803b0510e1c18af7ed2b016b
Alejo <vera.alejo0903@gmail.com>
2020-11-24 11:14:55 -0300

    Primeros archivos

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.css';
+import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
+
+function Header() {
+    return (
+        <div className="header">
+            <PersonIcon />
+            <img className="header__logo" src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png" alt="Tinder Logo" />
+            <ForumIcon />
+        </div>
+    )
+}
+
+export default Header
