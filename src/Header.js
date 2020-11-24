5817cda437bb1d62a180cad2c1e71df4ce98147e
Alejo <vera.alejo0903@gmail.com>
2020-11-24 11:24:48 -0300

    Last changes Header

--- src/Header.js
+++ src/Header.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './Header.css';
 import PersonIcon from '@material-ui/icons/Person';
 import ForumIcon from '@material-ui/icons/Forum';
+import IconButton from '@material-ui/core/IconButton'
 
 function Header() {
     return (
         <div className="header">
-            <PersonIcon fontSize="large" />
+            <IconButton>
+                <PersonIcon className="header__icons" fontSize="large" />
+            </IconButton>
             <img className="header__logo" src="https://1000logos.net/wp-content/uploads/2018/07/tinder-logo.png" alt="Tinder Logo" />
-            <ForumIcon fontSize="large" />
+            <IconButton>
+                <ForumIcon className="header__icons" fontSize="large" />
+            </IconButton>
         </div>
     )
 }
