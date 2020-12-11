ef3c63e0f8b25b13f0b8dc0dd4e240dd1f83a4e2
Alejo <vera.alejo0903@gmail.com>
2020-12-11 10:49:23 -0300

    Adding things

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React from 'react';
 import Header from './Header';
+import TinderCards from './TinderCards';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
-     <Header />
+      <Header />
+      <Router>
+        <Switch>
+          <Route path="/chat">
+            <p>Hola</p>
+          </Route>
+          <Route path="/">
+            <TinderCards />
+          </Route>
+        {/* <Header /> */}
+        </Switch>
+      </Router>
+
+
     </div>
   );
 }
