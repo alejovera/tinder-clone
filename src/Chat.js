92cfcff41b0d0cfb3f66819d5b1a21f8133b13e1
Alejo <vera.alejo0903@gmail.com>
2021-03-12 10:25:39 -0300

    Cambios en Chat

--- src/Chat.js
+++ src/Chat.js
@@ -1,15 +1,5 @@
 import React from 'react';
 import './Chat.css';
-<<<<<<< HEAD
-import Avatar from '@material-ui/core/Avatar';
-import { Link } from 'react-router-dom';
-
-function Chat({ name, message, profilePic, timestamp }) {
-    return (
-        <Link to={`/chat/${name}`}>  
-          <div className="chat">
-                <Avatar className='chat__image' alt={name} src={profilePic} />
-=======
 import Avatar from "@material-ui/core/Avatar";
 import { Link } from 'react-router-dom';
 
@@ -18,7 +8,6 @@ function Chat({name, message, timestamp, profilePic}) {
         <Link to={`/chat/${name}`}> 
             <div className="chat">
                 <Avatar className="chat__image" alt={name} src={profilePic} />
->>>>>>> version2
                 <div className="chat__details">
                     <h2>{name}</h2>
                     <p>{message}</p>
